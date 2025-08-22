75233e8c5857fbb9e791110b5ac1f6867a6860ef
noyelsebastian <noyelaniyara@gmail.com>
2025-08-22 05:56:00 +0000

    Deploying to gh-pages from @ noyelsebastian/noyelsebastian.github.io@a8c7b20d88bcf54cced3a1b4029ebd3aab9210d2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -459,35 +459,35 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6E%6F%79%65%6C%61%6E%69%79%61%72%61@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://www.linkedin.com/in/www.linkedin.com/in/noyel-sebastian-501a87175", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-orcid',
+        title: 'ORCID',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://orcid.org/https://orcid.org/0009-0009-4909-6342", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=7KxhPfUAAAAJ", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
+        id: 'social-x',
+        title: 'X',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://twitter.com/noyelaniyara", "_blank");
         },
       },{
       id: 'light-theme',
