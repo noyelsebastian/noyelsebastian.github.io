8c1fd5654ac26a766fff9ace2f17e3f0cd36b6f3
noyelsebastian <noyelaniyara@gmail.com>
2025-08-22 07:27:49 +0000

    Deploying to gh-pages from @ noyelsebastian/noyelsebastian.github.io@8e1b70dda46aace6731db8e8183b260035d3a8fe 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
+        },{id: "nav-resources",
+          title: "Resources",
+          description: "A growing collection of your cool projects.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
